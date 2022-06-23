a6a5eb3ffb292b5e360d4b34b73a65dd8827c0c6
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 16:27:03 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Switch, Route, Navlink } from 'react-router-dom'; 
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'; 
 import ListPage from './ListPage';
 import EndangeredDetails from './EndangeredDetails';
 
@@ -10,11 +10,6 @@ function App() {
     <Router>
       <div className="App">
         <header><h1>WELCOME</h1></header>
-        <ul>
-          <li>
-            <Navlink exact activeClassName="active-link" to="/">List Page</Navlink>
-          </li>
-        </ul>
         <Switch>
           <Route exact path="/">
             <ListPage />
