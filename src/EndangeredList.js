4eb489c355c6d011cb26136e5ca70be86885aa76
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 17:38:56 -0600

    list page

--- src/EndangeredList.js
+++ src/EndangeredList.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function EndangeredList() {
+export default function EndangeredList({ animals }) {
   return (
-    <div>EndangeredList</div>
-  )
+    <div className='animal-list'>
+      {
+        animals.map((animal, i) =>
+          <Link key={animals.common_name + i + animals.id} to={`/animals/${animals.id}`}>
+            <div className='danger'>
+              <h3>{animal.common_name}</h3>
+              <p>{animal.Order}</p>
+              <p>{animal.date_extinction}</p>
+              <p>{animal.former_range}</p>
+            </div>
+          </Link>)
+      }
+    </div>
+  );
 }
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import EndangeredList from "./EndangeredList";
+import { getAnimalsAndCount } from "./services/Fetch-Utils";
+import Pagination from "./Pagination";
+
+const PER_PAGE = 35;
 
 export default function ListPage() {
+    const [animal, setAnimal] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalAnimals, setTotalAnimals] useState(1);
+
+    useEffect(() => {
+        async function doFetch() {
+            const from = currentPage * PER_PAGE;
+            const to = (( currentPage + 1) * PER_PAGE) + 1;
+            const animalsAndCount = await getAnimalsAndCount(from, to);
+
+            setTotalAnimals(animalsAndCount); setAnimal(animalsAndCount.data)
+        }
+        doFetch();
+    }, [currentPage]);
+
+    const lastPage = Math.floor(totalAnimals / PER_PAGE);
+
+
   return (
-    <div>ListPage</div>
+    <div>
+        <Pagination 
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        lastPage={lastPage} />
+        <EndangeredList  animal={animal}/>
+    </div>
   );
 }
