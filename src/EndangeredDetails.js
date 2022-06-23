708114ed7b6f3c3b717625715ae21dc982eca6c9
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 16:21:09 -0600

    endangered details

--- src/EndangeredDetails.js
+++ src/EndangeredDetails.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getAnimalById } from './services/Fetch-Utils';
+
 
 export default function EndangeredDetails() {
+  const params = useParams();
+  const [endangered, setEndangered] = useState({});
+
+  useEffect(() => {
+    async function fetch() {
+      const data = await getAnimalById(params.id);
+      setEndangered(data);
+    }
+    fetch();
+  }, [params.id]);
+
   return (
-    <div>EndangeredDetails</div>
-  )
+    <div className='animal-detail'>
+      <h2>{endangered}</h2>
+      <a href={endangered.link} target="_blank" rel="noreferrer">ENTRY</a>
+      <p>{endangered}</p>
+    </div>
+  );
 }
