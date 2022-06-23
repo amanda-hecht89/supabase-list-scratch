ae07a54ed36f2eec7fd22194c38cf84837a0246e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 12:01:23 -0600

    app. js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
+import { BrowserRouter as Router, Switch, Route, Navlink } from 'react-router-dom'; 
+import ListPage from './ListPage';
+import EndangeredDetails from './EndangeredDetails';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header><h1>WELCOME</h1></header>
-    </div>
+    <Router>
+      <div className="App">
+        <header><h1>WELCOME</h1></header>
+        <ul>
+          <li>
+            <Navlink exact activeClassName="active-link" to="/">List Page</Navlink>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <ListPage />
+          </Route>
+          <Route exact path="/animals/:id">
+            <EndangeredDetails />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
